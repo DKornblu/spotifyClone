bb2661832f823fbcd08a6bb55f3febb6d61ee38c
eve seni <senieve@gmail.com>
2023-12-12 13:15:43 -0500

    tailwind installed

--- src/Components/RightPane.jsx
+++ src/Components/RightPane.jsx
@@ -1,13 +1,16 @@
-import React from 'react'
-import RecommendedList from "./RecommendedList"
+import React from "react";
+import RecommendedList from "./RecommendedList";
+import NavbarElement from "./NavbarElement";
 
 const RightPane = () => {
-    return <>
-    <section className="rightPane">
+  return (
+    <>
+      <section className="rightPane">
         Right Pane
-        <RecommendedList/>
-        </section>
+        <NavbarElement />
+      </section>
     </>
-}
+  );
+};
 
 export default RightPane;
