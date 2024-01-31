0ac2e5a59e6a5a947e4f5f88ce10700472077e43
eve seni <senieve@gmail.com>
2024-01-31 11:56:38 -0500

    search page

---
+++ src/Pages/SearchPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SearchPage = () => {
+  return (
+    <>
+      <section className="flex-1 bg-slate-600">hello</section>
+    </>
+  );
+};
+
+export default SearchPage;
